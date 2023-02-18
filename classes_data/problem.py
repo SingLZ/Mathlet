93cc89d4f5e6666ee7fa05df4eee0952d0f3a8a3
hn437 <hn437@drexel.edu>
2023-02-18 12:39:15 -0500

    expanded, tested, and with randomizable samples testing

--- classes_data/problem.py
+++ classes_data/problem.py
@@ -1,9 +1,35 @@
+from abc import ABC, abstractmethod
+#from GradeLevel import GradeLevel, PreSchool
 from dataclasses import dataclass
 
 @dataclass
-class Problem:
+class Problem(ABC):
     Equation: str
     Steps: list | set
-    Answer: float # includes inf/-inf
-
+    GradeLevel: any
+    Answer: float | str # includes inf/-inf
     CurrentStep = 0 # index in Steps
+
+    def advanceAndGet(self):
+        CurrentStepData = self.Steps[self.CurrentStep]
+        self.CurrentStep += 1
+        return CurrentStepData
+    
+    def isDone(self):
+        return self.CurrentStep == len(self.Steps)
+
+def manufactureArithmeticProblem():
+    problem = Problem("2x + 3 = 7", [("-3", "2x + 3 - 3 = 7 - 3", "2x = 7 - 3", "2x = 4"), ("/2", "x = 4/2")], 'PreSchool', "x = 2")
+    return problem
+
+# sample problem
+if __name__ == "__main__":
+    problem = manufactureArithmeticProblem()
+    while not problem.isDone():
+        print(problem.Equation)
+        if input("type anything to go to next step > ").lower() == "quit":
+            exit()
+        step = problem.advanceAndGet()
+        for i in range(1, len(step)):
+            print(step[i])
+    print(f"The solution is: {problem.Answer}")
