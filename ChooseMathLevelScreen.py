63bbc363d8e4e930f425750e4b5c3481b3532d34
unknown <sz595@drexel.edu>
2023-02-23 22:10:25 -0500

    User Interface #2 for Choose Math Level Screen there is a .py file and a .kv file for this code

---
+++ ChooseMathLevelScreen.py
@@ -0,0 +1,34 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
+
+#Define different Screens
+class LevelWindow(Screen):
+    pass
+
+class Elementary(Screen):
+    pass
+
+class MiddleSchool(Screen):
+    pass
+
+class HighSchool(Screen):
+    pass
+
+class College(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file('new_window.kv')
+
+class LevelApp(App):
+    def build(self):
+        Window.clearcolor = (255, 255, 255)
+        return kv
+    
+if __name__ == '__main__':
+    LevelApp().run()
