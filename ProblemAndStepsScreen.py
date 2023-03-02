e50bc2f20961c15be41d4c86cac46aa1ddf00392
Clara Shao <cs3834@drexel.edu>
2023-03-01 23:53:11 -0500

    Renamed files, created two layouts for questions

---
+++ ProblemAndStepsScreen.py
@@ -0,0 +1,55 @@
+import kivy
+kivy.require('2.1.0')
+
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.metrics import dp
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+class ProblemCards(Screen):
+    strA = StringProperty("A")
+    strB = StringProperty("B")
+    strC = StringProperty("C")
+    strD = StringProperty("D")
+    question = StringProperty("Question")
+    def on_button_click(self):
+        self.strA = "Transition to Feedback Screen"
+    def clicked(self):
+        self.strB = "Transition to Feedback Screen A Super Duper Duper Long Answer"
+
+class WindowManager(ScreenManager):
+    pass
+
+#oneFour = Builder.load_file('OneFourProblemCards.kv')
+twoTwo = Builder.load_file('TwoTwoProblemCards.kv')
+#kv = Builder.load_file('new_window.kv')
+
+class TwoTwoProblemCardsApp(App):
+    def build(self):
+        Window.clearcolor = (255, 255, 255)
+        return twoTwo
+    
+class OneFourProblemCardsApp(App):
+    def build(self):
+        Window.clearcolor = (255, 255, 255)
+        return oneFour 
+
+if __name__ == '__main__':
+    TwoTwoProblemCardsApp().run()
+
+'''
+Note for testing:
+    Change line 40 to 
+    TwoTwoProblemCardsApp().run() or
+    OneFourProblemCardsApp().run()
+    and
+    Comment out lines 25 or 26 depending on which
+    file you want to check and uncomment the other
+    line
+
+    For example, if we want to see the Two/Two
+    format, switch line 40 to TwoTwoProblemCardsApp().run()
+    and comment out line 25 and uncomment line 26
+'''
