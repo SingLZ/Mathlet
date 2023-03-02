35da5cb2a6145843d0d2be5dc9c7a0f877db855d
Sammy <sz595@drexel.edu>
2023-03-02 14:23:10 -0500

    Linked 2x2 Card Layout Screen with Topics Screen

--- ChooseMathLevelScreen.py
+++ ChooseMathLevelScreen.py
@@ -1,12 +1,25 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.image import Image
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
+from kivy.properties import StringProperty
 
 #Define different Screens
 class LevelWindow(Screen):
     pass
+
+class ProblemCards(Screen):
+    strA = StringProperty("A")
+    strB = StringProperty("B")
+    strC = StringProperty("C")
+    strD = StringProperty("D")
+    question = StringProperty("Question")
+    def on_button_click(self):
+        self.strA = "Transition to Feedback Screen"
+    def clicked(self):
+        self.strB = "Transition to Feedback Screen A Super Duper Duper Long Answer"
 
 class Elementary(Screen):
     pass
