34837e1f19ce0350ea12bcf6003af6af20ad751c
Sammy <sz595@drexel.edu>
2023-03-07 15:22:04 -0500

    Added image template on Problem Layout screen for the 5 buttons and updated the back button on problem layout screen to always root to previous screen.

--- ChooseMathLevelScreen.py
+++ ChooseMathLevelScreen.py
@@ -1,3 +1,6 @@
+import kivy
+kivy.require('2.1.0')
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.image import Image
@@ -5,6 +8,9 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 from kivy.properties import StringProperty
+# removes red dot when you left click
+from kivy.config import Config
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 
 #Define different Screens
 class LevelWindow(Screen):
@@ -36,9 +42,19 @@ class College(Screen):
 class WindowManager(ScreenManager):
     pass
 
+# for back button to return to previous screen
+class RootWidget(ScreenManager):
+    pass
+
 kv = Builder.load_file('new_window.kv')
+# Builder.load_file("ProblemCards.kv")
 
 class LevelApp(App):
+    # for back button in ProblemCards.kv to return to previous screen
+    def __init__(self, **kwargs):
+        super(LevelApp, self).__init__(**kwargs)
+        self.previous_screen = "" 
+
     def build(self):
         Window.clearcolor = (255, 255, 255)
         return kv
