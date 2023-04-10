48e9c4c9d94c067c55d68c96e7783e3249343689
hn437 <hn437@drexel.edu>
2023-04-10 16:06:02 -0400

    added padding, visual improvement

--- mathimg.py
+++ mathimg.py
@@ -2,18 +2,20 @@ from matplotlib import font_manager, figure
 import matplotlib as mpl
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
+mpl.rcParams.update(mpl.rcParamsDefault) 
 mpl.rcParams['savefig.transparent'] = True
-mpl.rcParams['text.usetex'] = True
+mpl.rcParams['text.usetex'] = True # turn off temporarily if causing problems
+mpl.use('Agg') # Set backend to Agg
 
 texFont = font_manager.FontProperties(size=30, family='serif', math_fontfamily='cm')
 
 def make_img(txt: str, fileName: str = 'output', format: str = 'png', customDpi: int = 300, color: str = 'White'):
-    mpl.use('Agg') # Set backend to Agg
     fig = figure.Figure(figsize=(4, 2))
     FigureCanvas(fig)
+
     fig.text(0.5, 0.5, txt, color=color, fontsize=texFont.get_size_in_points(), fontproperties=texFont, verticalalignment='center', horizontalalignment='center')
     fileName += f'.{format}'
-    fig.savefig(fileName, dpi=customDpi, transparent=True, bbox_inches='tight', pad_inches=0)
+    fig.savefig(fileName, dpi=customDpi, transparent=True, bbox_inches='tight', pad_inches=0.1) # adjust padding here
     return fileName
 
 def render_output(new_txt: list | str, fileName='output'):
