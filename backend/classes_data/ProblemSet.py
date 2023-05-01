b4e8e17f95b73e1eb0160d0af4e384d075ed29f4
hn437 <hn437@drexel.edu>
2023-05-01 18:56:11 -0400

    +score attribute,+fixed return result typing,+save

--- backend/classes_data/ProblemSet.py
+++ backend/classes_data/ProblemSet.py
@@ -4,6 +4,7 @@ from .problem import Problem
 class ProblemSet():
     problems: set
     current: int = 0
+    high_score_index: int = 0
 
     def __init__(self, *args) -> None:
         if isinstance(args[0], set):
@@ -17,19 +18,26 @@ class ProblemSet():
     def getProblems(self):
         return self.problems
 
-    def getCurrentProblem(self): # may become deprecated
+    def getCurrentProblem(self):
         return self.problems[self.current]
 
-    def next(self): # may become deprecated
+    def next(self):
         if self.current == len(self.problems):
             return False
         self.current += 1
         return True
     
-    def getCurrentProgress(self) -> float: # from 0 to 1 (float)
+    def save_score(self, num: int = None): # for saving
+        if not num:
+            num = self.current 
+        if 0 <= num <= len(self.problems):
+            self.high_score_index = num
+        raise IndexError
+    
+    def getCurrentProgress(self) -> str: # from 0 to 1 (float)
         return '%.02f' % self.current/len(self.problems)
     
-    def cycle(self): # USE
+    def cycle(self):
         for index in range(0, len(self.problems)):
             self.current = index
             yield self.problems[index]
