24362e98d7c4b8a98f4151186081b13d8de378b0
hn437 <hn437@drexel.edu>
2023-05-01 20:31:09 -0400

    Added sanity debug check just in case

--- mathimg.py
+++ mathimg.py
@@ -13,6 +13,9 @@ def make_img(txt: str, fileName: str = 'output', format: str = 'png', customDpi:
     fig = figure.Figure(figsize=(4, 2))
     FigureCanvas(fig)
 
+    if type(txt) != str:
+        raise Exception("invalid type", type(txt))
+
     fig.text(0.5, 0.5, txt, color=color, fontsize=texFont.get_size_in_points(), fontproperties=texFont, verticalalignment='center', horizontalalignment='center')
     fileName += f'.{format}'
     fig.savefig(fileName, dpi=customDpi, transparent=True, bbox_inches='tight', pad_inches=0.1) # adjust padding here
