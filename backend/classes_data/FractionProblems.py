08c8c04b35abfaf5de366692f4ba0720f414916a
hn437 <hn437@drexel.edu>
2023-05-01 20:32:09 -0400

    Reformatted examples to work with new problem.py

--- backend/classes_data/FractionProblems.py
+++ backend/classes_data/FractionProblems.py
@@ -3,17 +3,28 @@ from .Fraction import Fraction as Frac
 
 problems = [
     Problem(Equation=f'{Frac(1,2)} + {Frac(1,3)}', 
-        Steps=[Step(rf'{Frac(3, 6)} + {Frac(2, 6)}', 
-                    rf'Multiply {Frac(1,2)} by {Frac(3,3)} and {Frac(1,3)} by {Frac(2,2)}', 
-                    (rf'Multiply {Frac(1,2)} by {Frac(2,2)} and {Frac(1,3)} by {Frac(3,3)}', rf'Multiply {Frac(1,2)} by {Frac(2,1)} and {Frac(1,3)} by {Frac(3,1)}', rf'Divide {Frac(1,2)} by {Frac(3,3)} and {Frac(1,3)} by {Frac(2,1)}'),
-                    feedback='Step 1'
+        Steps=[Step(result=rf'{Frac(3, 6)} + {Frac(2, 6)}', 
+                    step=rf'Multiply {Frac(1,2)} by {Frac(3,3)} and {Frac(1,3)} by {Frac(2,2)}', 
+                    wrong_steps=(
+                        Step(step=rf'Multiply {Frac(1,2)} by {Frac(2,2)} and {Frac(1,3)} by {Frac(3,3)}', feedback='Wrong Step'), 
+                        Step(step=rf'Multiply {Frac(1,2)} by {Frac(2,1)} and {Frac(1,3)} by {Frac(3,1)}', feedback='Wrong Step'), 
+                        Step(step=rf'Divide {Frac(1,2)} by {Frac(3,3)} and {Frac(1,3)} by {Frac(2,1)}', feedback='Wrong Step')
                     ),
-                Step(r'$\frac{3+2}{6}$', 'Combine fractions due to same denominator',
-                     ('Add denominators and numerators', rf'Multiply {Frac(1,2)} by {Frac(3,3)} and {Frac(1,3)} by {Frac(2,2)}', 'No more work can be done'),
-                     feedback='Step 2'),
-                Step(repr(Frac(5,6)), 'Complete sum', 
-                     (rf'Multiply by {Frac(6,6)}', rf'Convert into {Frac(3,6)} + {Frac(2,6)}', r'No more work can be done'),
-                     feedback='Step 3')
+                    feedback='Step 2'
+                    ),
+                Step(result=r'$\frac{3+2}{6}$', step='Combine fractions due to same denominator',
+                     wrong_steps=(
+                        Step(step='Add denominators and numerators', feedback='Wrong Step'), 
+                        Step(step=rf'Multiply {Frac(1,2)} by {Frac(3,3)} and {Frac(1,3)} by {Frac(2,2)}', feedback='Wrong Step'), 
+                        Step(step='No more work can be done', feedback='Wrong Step')
+                        ),
+                     feedback='Step 3'),
+                Step(result=repr(Frac(5,6)), step='Complete sum', 
+                     wrong_steps=(Step(rf'Multiply by {Frac(6,6)}', feedback='Wrong Step'), 
+                                  Step(rf'Convert into {Frac(3,6)} + {Frac(2,6)}', feedback='Wrong Step'), 
+                                  Step(r'No more work can be done', feedback='Wrong Step')
+                                  ),
+                     feedback='Step 4')
                 ],
         )
     #Problem([f'{Frac(1, 2)} + {Frac(1, 3)}', f'{Frac(3, 6)} + {Frac(2, 6)}', r'$\frac{3+2}{6}$', repr(Frac(5, 6))], 
@@ -22,3 +33,6 @@ problems = [
 ]
 
 trial = problems[0]
+if __name__ == "__main__": # test only
+    for part in trial.getCurrentWrongSteps():
+        print(part.feedback)
