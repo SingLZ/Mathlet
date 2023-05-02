b104d97cbe127a0714cd32c28abbd6ac5081fdbb
hn437 <hn437@drexel.edu>
2023-05-01 20:31:36 -0400

    Added caching ability and serialization/load funcs

--- TopicCenter.py
+++ TopicCenter.py
@@ -7,7 +7,7 @@ class TopicCenter():
     def __init__(self, sets: dict):
         self.sets = sets
         self.current_set: ProblemSet = None
-        self.topic_access_cache = {}
+        self.topic_access_cache = []
 
     def getCurrentSet(self):
         return self.current_set
@@ -17,18 +17,23 @@ class TopicCenter():
 
     def selectSet(self, set_name: str) -> ProblemSet:
         self.current_set = self.sets[set_name]
-        self.topic_access_cache[set_name] = True
+        try:
+            self.topic_access_cache.index(set_name)
+        except:
+            pass
+        else:
+            self.topic_access_cache.append(set_name)
         return self.current_set
 
     def cycleProblems(self):
         return self.current_set.cycle()
     
-    def getCurrentProblem(self):
+    def getCurrentProblem(self) -> Problem:
         return self.getCurrentSet().getCurrentProblem()
     
-    def saveAndGet(self):
-        for set in self.topic_access_cache:
-            set.save_score()
+    def save(self):
+        for set_name in self.topic_access_cache:
+            self.sets[set_name].save_score()
         print("Saved, progress is: " + self.calculateCompositeCompletion())
         return self.sets
 
@@ -38,17 +43,17 @@ class TopicCenter():
     def calculateCompositeCompletion(self) -> str: # average
         sum = 0
         total = 0
-        for problemSet in self.sets:
-            sum += problemSet.current
+        for problemSet in self.getSets().values():
+            sum += problemSet.get_score()
             total += len(problemSet.getProblems())
         
-        return "%.02f" % sum/total
+        return "%.02f" % (sum/total)
     
 from backend.classes_data.FractionProblems import problems
 main = TopicCenter(
     sets={
         'Fractions': ProblemSet(
-            problems[0] # add here
+            (problems[0]) # add here
         )
     }
 )
@@ -56,16 +61,18 @@ main = TopicCenter(
 '''
 main = TopicCenter(
     {
-        'Topic': ProblemSet(
+        'Topic': ProblemSet((
             Problem(...),
-            Problem(...),
+            Problem(...)),
             ...
         ),
         'MoreTopics': ProblemSet(
-            ...
+            (...)
         )
     }
 )
+
+encase arguments in problem sets with sets
 '''
 
 if __name__ == "__main__":
