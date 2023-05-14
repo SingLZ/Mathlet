c8f546b5fc49db7afba2c5a724293fdd038b3bbc
hn437 <hn437@drexel.edu>
2023-05-14 00:37:16 -0400

    commented out faulty ProblemSet

--- backend/classes_data/FractionProblems.py
+++ backend/classes_data/FractionProblems.py
@@ -81,27 +81,29 @@ problems = [
                 ]
             ), 
             #r'$\frac{3+2}{6}$'
-    Problem(Equation=r'$\frac{x^{2} + 6x + 8}{x^{2} + 7x + 10}$', 
-        Steps=[Step(result = r'$\frac{(x+4)(x+2)}{x^{2} + 7x + 10}$',
-                    step = r'$\frac{(x+4)(x+2)}{x^{2} + 7x + 10}$',
-                    feedback='Correct. We can factor the numberator\nto simplify the fraction',
-                    wrong_steps=
-                        (Step(step= r'$\frac{(x+8)(x+1)}{x^{2} + 7x + 10}$',
-                              feedback='Incorrect. 8x + 1x does not equal 6x.'),
-                         Step(step= r'$\frac{(x-4)(x-2)}{x^{2} + 7x + 10}$',
-                              feedback=r'Incorrect. This is equal to $x^{2}-6x+8$''\nwhich is not the same numerator'),
-                         Step(step= r'$\frac{(x+4)(x+4)}{x^{2} + 7x + 10}$',
-                              feedback='You factored incorrectly.')
-                        ),
-                    ),
-                Step(r'$\frac{3+2}{6}$', 'Combine fractions due to same denominator',
-                     ('Add denominators and numerators', rf'Multiply {Frac(1,2)} by {Frac(3,3)} and {Frac(1,3)} by {Frac(2,2)}', 'No more work can be done'),
-                     feedback='Step 2'),
-                Step(repr(Frac(5,6)), 'Complete sum', 
-                     (rf'Multiply by {Frac(6,6)}', rf'Convert into {Frac(3,6)} + {Frac(2,6)}', r'No more work can be done'),
-                     feedback='Step 3')
-                ],
-        )
+            
+    #Problem(Equation=r'$\frac{x^{2} + 6x + 8}{x^{2} + 7x + 10}$', 
+        #Steps=[Step(result = r'$\frac{(x+4)(x+2)}{x^{2} + 7x + 10}$',
+                    #step = r'$\frac{(x+4)(x+2)}{x^{2} + 7x + 10}$',
+                    #feedback='Correct. We can factor the numerator\nto simplify the fraction',
+                    #wrong_steps=
+                        #(Step(step= r'$\frac{(x+8)(x+1)}{x^{2} + 7x + 10}$',
+                              #feedback='Incorrect. 8x + 1x does not equal 6x.'),
+                         #Step(step= r'$\frac{(x-4)(x-2)}{x^{2} + 7x + 10}$',
+                              #feedback=r'Incorrect. This is equal to $x^{2}-6x+8$''\nwhich is not the same numerator'),
+                         #Step(step= r'$\frac{(x+4)(x+4)}{x^{2} + 7x + 10}$',
+                              #feedback='You factored incorrectly.')
+                        #),
+                    #),
+                #Step(r'$\frac{3+2}{6}$', 'Combine fractions due to same denominator',
+                     #('Add denominators and numerators', rf'Multiply {Frac(1,2)} by {Frac(3,3)} and {Frac(1,3)} by {Frac(2,2)}', 'No more work can be done'),
+                     #feedback='Step 2'),
+                #Step(repr(Frac(5,6)), 'Complete sum', 
+                     #(rf'Multiply by {Frac(6,6)}', rf'Convert into {Frac(3,6)} + {Frac(2,6)}', r'No more work can be done'),
+                     #feedback='Step 3')
+                #],
+        #)
+        
     #Problem([f'{Frac(1, 2)} + {Frac(1, 3)}', f'{Frac(3, 6)} + {Frac(2, 6)}', r'$\frac{3+2}{6}$', repr(Frac(5, 6))], 
             #[r'Multiply $\frac{1}{2}$ by $\frac{3}{3}$ and $\frac{1}{3}$ by $\frac{2}{2}$', 'Combine fractions due to same denominator', 'Complete sum'],
             #[(r'Multiply $\frac{1}{2}$ by $\frac{2}{2}$ and $\frac{1}{3}$ by $\frac{3}{3}$', r'Multiply $\frac{1}{2}$ by $\frac{2}{1}$ and $\frac{1}{3}$ by $\frac{3}{1}$', r'Divide $\frac{1}{2}$ by $\frac{3}{3}$ and $\frac{1}{3}$ by $\frac{2}{1}$'), ('Add denominators and numerators', r'Multiply $\frac{1}{2}$ by $\frac{3}{3}$ and $\frac{1}{3}$ by $\frac{2}{2}$', 'No more work can be done'), (r'Multiply by $\frac{6}{6}$', r'Convert into $\frac{3}{6}$ + $\frac{2}{6}$', r'No more work can be done')])
