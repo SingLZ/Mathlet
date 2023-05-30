4636a6defd423485d60c99e610042c0f1e7dfc79
hn437 <hn437@drexel.edu>
2023-05-30 00:12:32 -0400

    text wrapping function added and incorporated

--- mathimg.py
+++ mathimg.py
@@ -6,6 +6,8 @@ import io
 import numpy as np
 import PIL.Image as Image
 
+from timeit import default_timer
+
 font_manager.FontProperties('cm')
 
 mpl.rcParams.update(mpl.rcParamsDefault) 
@@ -15,21 +17,80 @@ mpl.use('Agg') # Set backend to Agg
 
 texFont = font_manager.FontProperties(size=30, family='serif', math_fontfamily='cm')
 
-from timeit import default_timer
-
 fig = figure.Figure(figsize=(4, 2))
+fig.patch.set_alpha(0)
 FigureCanvas(fig)
 
-def make_img(txt: str, fileName: str = 'output', format: str = 'png', customDpi: int = 300, color: str = 'White'):
+import re
+def fill(text, width=70):
     then = default_timer()
+    lines = []
+    current_line = ""
+    line_length = 0
+    in_control_sequence = False
+    control_sequence_length = 0
 
+    for char in text:
+        if char == "$":
+            if in_control_sequence:
+                # Adjust line length by the visual length of the control sequence
+                line_length -= control_sequence_length
+                control_sequence_length = 0
+            in_control_sequence = not in_control_sequence
+
+        if in_control_sequence:
+            control_sequence_length += 1
+            current_line += char
+        else:
+            current_line += char
+            line_length += 1
+
+            # Check if the current line exceeds the width
+            if line_length >= width:
+                # If the line ends with a space, remove it
+                current_line = current_line.rstrip()
+                if current_line[-1] == " ":
+                    current_line = current_line[:-1]
+
+                # Check if the last word can fit in the remaining space
+                last_word_match = re.search(r'\b\w+$', current_line)
+                if last_word_match:
+                    last_word = last_word_match.group()
+                    word_length = len(last_word)
+
+                    # Check if the last word can fit on the next line
+                    if word_length > width:
+                        # If the word itself is longer than the width, split it across lines
+                        lines.append(current_line.rstrip())
+                        current_line = last_word
+                    else:
+                        # Move the last word to the next line if it doesn't fit
+                        current_line = current_line[:last_word_match.start()].rstrip()
+                        lines.append(current_line)
+                        current_line = last_word
+                else:
+                    # Append the current line to the list of lines
+                    lines.append(current_line.rstrip())
+                    current_line = ""
+                line_length = len(current_line)
+
+    if current_line:
+        lines.append(current_line)
+
+    result = "\n".join(lines)
+    now = default_timer() - then
+    if now > 0.2:
+        print(f"Text wrap/fill bottleneck detected. Time: {now}")
+
+    return result
+
+def make_img(txt: str, fileName: str = 'output', format: str = 'png', customDpi: int = 300, color: str = 'White'):
+    txt = fill(txt, 20)
     fig.text(0.5, 0.5, txt, color=color, fontsize=texFont.get_size_in_points(), fontproperties=texFont, verticalalignment='center', horizontalalignment='center')
     fileName += f'.{format}'
     
     fig.savefig(fileName, dpi=customDpi, transparent=True, bbox_inches='tight', pad_inches=0.1) # adjust padding here
     fig.clear()
-    if default_timer() - then > 0.6:
-        print('image load time:', '%.4f' % (default_timer() - then))
     return fileName
 
 # make_img is the fastest, but make_bytes is the best for network connectivity, so only use if transferring images
@@ -55,26 +116,17 @@ def render_output(new_txt: list | str, fileName='output'):
     return make_img(txt, fileName, 'png', 300, 'White')
 
 def pre_load():
-    make_img('', 'output', 'png')
-    make_img('', 'choice1', 'png')
-    make_img('', 'choice2', 'png')
-    make_img('', 'choice3', 'png')
-    make_img('', 'choice4', 'png')
+    make_img(' ', 'output', 'png')
+    make_img(' ', 'choice1', 'png')
+    make_img(' ', 'choice2', 'png')
+    make_img(' ', 'choice3', 'png')
+    make_img(' ', 'choice4', 'png')
 
 thread = threading.Thread(target=pre_load)
 thread.start()
 
 if __name__ == "__main__":
     thread.join()
-    from random import randint
-    randomized = r'$\frac{1}{2} + \frac{1}{3}$' #chr(randint(ord('a'), ord('z')))
-    then = default_timer()
-    make_bytes(randomized + ' $x^{2}$', 'output')
-    time1 = default_timer() - then
-
-    then = default_timer()
-    make_img(randomized + ' $x^{2}$', 'output')
-    
-    print(f'bytes: {time1}, image: {default_timer() - then}')
+    # empty
 
 #render_output([r'$\frac{1}{2} + \frac{1}{3}$', r'$\frac{1}{3} + \frac{4}{3}$', r'$\frac{x}{z} + \frac{2}{y}$'])
