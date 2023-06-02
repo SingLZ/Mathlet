a8fd0e9a24695452a46e88375991e61936e5d187
hn437 <hn437@drexel.edu>
2023-06-02 11:18:56 -0400

    comments changed

--- mathimg.py
+++ mathimg.py
@@ -30,10 +30,11 @@ def fill(text, width=70):
     in_control_sequence = False
     control_sequence_length = 0
 
+    # pseudo text fill/wrap, does not actually fill up to width, but compromises when words become long
+
     for char in text:
         if char == "$":
             if in_control_sequence:
-                # Adjust line length by the visual length of the control sequence
                 line_length -= control_sequence_length
                 control_sequence_length = 0
             in_control_sequence = not in_control_sequence
@@ -46,29 +47,22 @@ def fill(text, width=70):
             line_length += 1
 
             if line_length >= width:
-                # If the line ends with a space, remove it
-                #current_line = current_line.rstrip()
-                #if current_line[-1] == " ":
-                    #current_line = current_line[:-1]
-
-                # Check if the last word can fit in the remaining space
-                last_word_match = re.search(r'\b\w+$', current_line)
+                last_word_match = re.search(r'\b\w+$', current_line) # last word fits in remaining space
                 if last_word_match:
                     last_word = last_word_match.group()
                     word_length = len(last_word)
 
-                    # Check if the last word can fit on the next line
+                    # word fits on next line
                     if word_length > width:
-                        # If the word itself is longer than the width, split it across lines
+                        # if huge word, make it its own line
                         lines.append(current_line.rstrip())
                         current_line = last_word
                     else:
-                        # Move the last word to the next line if it doesn't fit
+                        # move last word to next line if unable to fit
                         current_line = current_line[:last_word_match.start()].rstrip()
                         lines.append(current_line)
                         current_line = last_word
                 else:
-                    # Append the current line to the list of lines
                     lines.append(current_line.rstrip())
                     current_line = ""
                 line_length = len(current_line)
